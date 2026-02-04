67d38017c4a15eaadae3f034f3ca4cda5423ca51
N25 <forngandyt@gmail.com>
2026-02-04 18:20:50 +0300

    Changed sql and add classes to processor, changed libraries

--- App/data.py
+++ App/data.py
@@ -4,6 +4,7 @@ import os
 DIR = "DB"
 DATABASE = "mydb.db"
 PATH = DIR + "/"+ DATABASE
+DB_NAME = "vocabulary"
 
 if not os.path.exists(DIR):
     os.mkdir(DIR)
@@ -14,28 +15,27 @@ class DB:
         self.conn = sql.connect(PATH)
         self.crs = self.conn.cursor()
         self.crs.execute(
-            """
-            CREATE TABLE IF NOT EXISTS vocabulary (
+            f"""
+            CREATE TABLE IF NOT EXISTS {DB_NAME} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             lemma VARCHAR(31) NOT NULL,
-            form VARCHAR(31) NOT NULL,
+            form VARCHAR(31) NOT NULL UNIQUE,
             part_of_speech VARCHAR(31) CHECK (part_of_speech IN ('ADJ', 'ADV', 'NOUN', 'NUM', 'VERB', 'PRON', 'OTHER')),
             role VARCHAR(31) 
             );
             """
             )
         self.crs.execute(
-            """
-            CREATE INDEX IF NOT EXISTS idx_lemma ON vocabulary(lemma);
+            f"""
+            CREATE INDEX IF NOT EXISTS idx_lemma ON {DB_NAME}(lemma);
             """
             )
-    
     
     def __del__(self)-> None:
         if self.conn:
             self.conn.close()
     
-    def execute_single(self, command:str) -> None:
+    def execute_query(self, command:str) -> None:
         try:
             self.crs.execute(command)
             self.conn.commit()
@@ -43,23 +43,13 @@ class DB:
         except sql.Error as e:
             print(f"(!) Error: {e}")
             self.conn.rollback()
-    
-    def execute_many(self, command:str, params:list[tuple]) -> None:
-        try:
-            self.crs.executemany(command, params)
-            self.conn.commit()
 
-        except sql.Error as e:
-            print(f"(!) Error: {e}")
-            self.conn.rollback()
-
-
-    def get_data(self, command:str, params:list[tuple]) -> list[tuple]:
+    def select_query(self, command:str) -> list[tuple]:
         data:list[tuple] = []
         if command.strip().upper().startswith("SELECT"):
 
             try:
-                self.crs.executemany(command, params)
+                self.crs.execute(command)
                 data = self.crs.fetchall()
 
             except sql.Error as e:
@@ -69,8 +59,4 @@ class DB:
             print("Incorrect 'SELECT' query")
 
         return data
-
-    def select_all(self)-> list[tuple]:
-        self.crs.execute("""SELECT * FROM vocabulary""")
-        return self.crs.fetchall()
         
--- App/processor.py
+++ App/processor.py
@@ -1,24 +1,24 @@
-import os
-import nltk
 import spacy
-from data import DB
+from data import DB, DB_NAME
+
+# Тут можешь его расширять
+class SQLhelper:
+    def __init__(self)->None:
+        self.db = DB()
+
+    def select_all(self) -> list[tuple]:
+        return self.db.execute_query(f"SELECT * from {DB_NAME}")
+    
+    def insert(self, values:list[tuple])->None:
+        for v in values:
+            self.db.execute_query(f"INSERT INTO {DB_NAME} (lemma, form, part_of_speech, role) VALUES {v}")
+
 
 class Parser:
 
     def __init__(self) -> None:
         self.nlp = spacy.load("en_core_web_sm")
-        self.db = DB()
-
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-
-        nltk_data_path = os.path.join(current_dir, 'nltk_data')
-
-        nltk.data.path.insert(0, nltk_data_path)
-
-        nltk.download('punkt', download_dir=nltk_data_path)
-        nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
-        nltk.download('wordnet', download_dir=nltk_data_path)
-        nltk.download('omw-1.4', download_dir=nltk_data_path)
+        self.sql = SQLhelper()
     
     def parse(text:str) -> None:
         pass
